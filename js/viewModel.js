0a8051ba82ce3d58d71811f984a3ac3ca6a4d5fc
DF4D133 <configurar@tecnosoft.com>
2016-11-08 15:39:15 -0300

    login

--- js/viewModel.js
+++ js/viewModel.js
@@ -1,11 +1,47 @@
-var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
+var ViewModel = function() {
+	var self = this;
+  	
+  	this.user = ko.observable("PP");
+    this.pass = ko.observable("12345");
  
-    this.fullName = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
+	self.validarUserCSS = function() {
+	    if (self.user().length == 0) {
+	      return "text-danger";
+	    }
+	}
+
+	self.validarUserContenido = ko.pureComputed(function() {
+	   if (self.user().length == 0) {	    
+	   	return "El campo User es obligatorio";
+		}
+
+	    return self.user();
+	}, this);
+
+	this.mostrarUserCampo = ko.pureComputed(function() {
+	   return self.user().length < 1;
+	}, this);
+
+
+	self.validarPassCSS = function() {
+	    if (self.pass().length == 0) {
+	      return "text-danger";
+	    }
+	}
+
+	self.validarPassContenido = ko.pureComputed(function() {
+	   if (self.pass().length == 0) {	    
+	   	return "El campo Pass es obligatorio";
+		}
+
+	    return self.pass();
+	}, this);
+
+	this.mostrarPassCampo = ko.pureComputed(function() {
+	   return self.pass().length < 1;
+	}, this);
+
+
 };
  
-ko.applyBindings(new ViewModel("Planet", "Earth"));
+ko.applyBindings(new ViewModel());
